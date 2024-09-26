e81198ff042f076b3f1a4728612aa0bd32136a1f
Ho Trung <khanhtrung14111999@gmail.com>
2024-09-26 22:56:37 +0300

    Sidebar component init for the logo and team name. Add .svg file for the logo section

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Navbar from './(components)/Navbar'
+import Sidebar from './(components)/Sidebar'
 
 
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
-            sidebar
-            {/* {sidebar} */}
+            <Sidebar />
             <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
-                {/* {navbar} */}
                 <Navbar />
                 {children}
             </main>

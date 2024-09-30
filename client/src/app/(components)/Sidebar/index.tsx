41cc1c7f8e599fe430b9d152bbfa70dc00d70de6
Ho Trung <khanhtrung14111999@gmail.com>
2024-09-30 23:03:52 +0300

    sidebarLink component

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+import { useAppDispatch, useAppSelector } from '@/app/redux'
 import { Icon, LockIcon, LucideIcon } from 'lucide-react'
 import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React, { useState } from 'react'
 
 const Sidebar = () => {
@@ -54,9 +57,30 @@ const SidebarLink = ({
     href,
     icon: Icon,
     label,
-    isCollapsed
+    // isCollapsed
 } : SidebarLinkProps) => {
-    
+    const pathname = usePathname()
+    const isActive = pathname === href || (pathname === "/" && href === "/dashboard");
+    const screenWidth = window.innerWidth;
+
+    const dispatch = useAppDispatch()
+    const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed)
+
+    return (
+        <Link href={href} className='w-full'>
+            <div className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 
+                ${isActive ? "bg-gray-100 text-white dark:bg-gray-600" : ""}`
+            }>
+                {isActive && (
+                    <div className='absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200' />
+                )}
+                <Icon className='h-6 w-6 text-gray-800 dark:text-gray-100' />
+                <span className={`font-medium text-gray-800 dark:text-gray-100 `}>
+                    {label}
+                </span>
+            </div>
+        </Link>
+    )
 }
 
 export default Sidebar
